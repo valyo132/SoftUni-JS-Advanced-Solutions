2908110968949aa0f71a666adbf236db5e6345ac
Valentin Markov <101991279+valyo132@users.noreply.github.com>
2024-02-24 11:22:59 +0200

    reorder

--- Arrays and Nested Arrays - Exercise/01. Print an Array with a Given Delimiter.js
+++
@@ -1,5 +0,0 @@
-function solve(input, str){
-    console.log(input.join(str));
-}
-
-solve(['one', 'two', '-']);
--- Arrays and Nested Arrays - Exercise/06. List Of Names.js
+++
@@ -1,7 +0,0 @@
-function solve(input){
-    let sorted = input.sort((a, b) => a.localeCompare(b));
-
-    for (let i = 0; i < input.length; i++) {
-        console.log(`${i + 1}.${sorted[i]}`);
-    }
-}
